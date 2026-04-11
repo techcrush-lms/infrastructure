c82eb35aa40bc006c7ebe4b810b4cbf8cf1d6e2c
oseghale ehichioya Israel <oseghale.i@turing.com>
2026-04-11 19:38:04 +0100

    fix: rename db_name parameter in RDS module to match provider requirements

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "this" {
   engine_version         = var.engine_version
   instance_class         = var.instance_class
   allocated_storage      = var.allocated_storage
-  name                   = var.db_name
+  db_name                = var.db_name
   username               = var.username
   password               = var.password
   skip_final_snapshot    = var.skip_final_snapshot
