4a17382770de1e1113e77439f653f7abc6a8943a
oseghale ehichioya Israel <oseghale.i@turing.com>
2026-04-28 10:18:03 +0100

    fix: cast production instance security group IDs to list to prevent indexing error

--- eic_bridge.tf
+++ eic_bridge.tf
@@ -5,6 +5,6 @@ resource "aws_security_group_rule" "ec2_allow_eic_ssh" {
   to_port                  = 22
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.eic_endpoint.id
-  security_group_id        = data.aws_instance.production.vpc_security_group_ids[0]
+  security_group_id        = tolist(data.aws_instance.production.vpc_security_group_ids)[0]
   description              = "Allow SSH from EC2 Instance Connect Endpoint"
 }
