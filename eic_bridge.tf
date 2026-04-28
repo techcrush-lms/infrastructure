d138bc74e64607cc188f5cfa6c76d17b0a8e3c30
oseghale ehichioya Israel <oseghale.i@turing.com>
2026-04-28 10:13:26 +0100

    feat: add security group rule to allow SSH access from EC2 Instance Connect Endpoint

---
+++ eic_bridge.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group_rule" "ec2_allow_eic_ssh" {
+  provider                 = aws.prod
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eic_endpoint.id
+  security_group_id        = data.aws_instance.production.vpc_security_group_ids[0]
+  description              = "Allow SSH from EC2 Instance Connect Endpoint"
+}
